255ea70ba9f8209c73df32c3446d800d56875e4b
Willem Gispen <willemgispen@outlook.com>
2022-11-16 16:40:18 +0100

    BCC15: count common particles between bonded sp4cs

--- src/clusters/BCC15.c
+++ src/clusters/BCC15.c
@@ -4,14 +4,13 @@
 #include "bonds.h"
 #include "tools.h"
 
-//!  An BCC15 cluster is an BCC15 cluster with six extra particles attached.
+//!  An BCC15 cluster is an arrangement of six sp4c clusters.
 /*!
 *  Find BCC15 clusters
-*  A BCC15 is a BCC15 cluster with six extra particles attached.
-*  Each extra particle is bonded to four of the non-spindle particles of the BCC15 cluster.
+*  A BCC15 is an arrangement of six sp4c clusters.
 *
-*  Cluster output: SBBBBBBBBEEEE
-*  Storage order: as for BCC15 x 9, extra_particles)
+*  Cluster output: SBBBBBBBBBBBB
+*  Storage order: central particle, other particles
 */
 
 void Clusters_GetBCC_15() {
@@ -21,73 +20,71 @@ void Clusters_GetBCC_15() {
     int trial[15];
     int clusSize=15;
 
-    for (i=0; i < nsp4c - 1; i++) {
-        for (j2=4; j2<6; j2++) {
-            for (j=0; j < nmem_sp4c[hcsp4c[i][j2]]; ++j) { // loop over all sp3c_j
-                if (mem_sp4c[hcsp4c[i][j2]][j] <= i) continue;
-                m=0;
-                for (k=4; k<6; k++) {
-                    for (l=4; l<6; l++) {
-                        if (hcsp4c[i][k] == hcsp4c[mem_sp4c[hcsp4c[i][j2]][j]][l]) {
-                            s_com= hcsp4c[i][k];
-                            m++;
-                        }
-                    }
-                }
-                if (m==0 || m>1) continue;
+    for (i=0; i < nsp4c - 1; i++) { // loop over sp4c
+        for (j2=4; j2<6; j2++) { // loop over spindles
+            int central_particle = hcsp4c[i][j2];
+            int *first_sp4c_cluster = hcsp4c[i];
 
-                flg=0;
+            // check that central spindle is part of at least six sp4c clusters
+            if (nmem_sp4c[central_particle] < 6) continue;
+
+            int sp4c_ids[6];
+            int idx_sp4c = 0;
+            sp4c_ids[idx_sp4c] = i;
+            idx_sp4c++;
+
+             // loop over sp4c's the central particle is part of
+            flg = 0;
+            for (j=0; j < nmem_sp4c[central_particle]; ++j) {
+                int other_sp4c_id = mem_sp4c[central_particle][j];
+                if (other_sp4c_id <= i) continue;
+                int *other_sp4c_cluster = hcsp4c[other_sp4c_id];
+
+                // make sure central particle is spindle of other sp4c
+                if (central_particle != other_sp4c_cluster[4] && central_particle != other_sp4c_cluster[5]) continue;
+
+                // count common particles
+                int ncom = 0;
                 for (k=0; k<6; k++) {
-                    if (hcsp4c[i][k] == s_com) continue;
                     for (l=0; l<6; l++) {
-                        if (hcsp4c[mem_sp4c[hcsp4c[i][j2]][j]][l] == s_com) continue;
-                        if (hcsp4c[i][k] == hcsp4c[mem_sp4c[hcsp4c[i][j2]][j]][l]) {
-                            flg=1;
-                            break;
+                        if (first_sp4c_cluster[k] == other_sp4c_cluster[l]) {
+                            ncom++;
                         }
                     }
-                    if (flg==1) break;
                 }
-                if (flg==1) continue;
+                if (ncom!=1 || ncom!=3) continue;
 
-                flg=0;
+                // count common ring particles
+                int ncom_ring = 0;
                 for (k=0; k<4; k++) {
-                    m=0;
                     for (l=0; l<4; l++) {
-                        if (Bonds_BondCheck(hcsp4c[i][k], hcsp4c[mem_sp4c[hcsp4c[i][j2]][j]][l])) {
-                            m++;
-                            if (m==2) {
-                                flg=1;
-                                break;
-                            }
+                        if (first_sp4c_cluster[k] == other_sp4c_cluster[l]) {
+                            ncom_ring++;
                         }
                     }
-                    if (flg==1) break;
                 }
-                if (flg==1) continue;
+                if (ncom-1 != ncom_ring) continue;
 
-                flg=0;
-                for (k=0; k<4; k++) {
-                    m=0;
-                    for (l=0; l<4; l++) {
-                        if (Bonds_BondCheck(hcsp4c[mem_sp4c[hcsp4c[i][j2]][j]][k], hcsp4c[i][l])) {
-                            m++;
-                            if (m==2) {
-                                flg=1;
-                                break;
-                            }
-                        }
-                    }
-                    if (flg==1) break;
+                // add to possible sp4c's
+                sp4c_ids[idx_sp4c] = i;
+                idx_sp4c++;
+                if (idx_sp4c == 6) {
+                    flg = 1;
+                    break;
                 }
-                if (flg==1) continue;
+            }
 
+            if (flg==1) continue;
+
+
+            for (j=0; j < nmem_sp4c[central_particle]; ++j) {
+                // sort particle ids to check uniqueness
                 trial[0]=s_com;
                 for (k=0; k<4; k++) {
-                    trial[k+1]= hcsp4c[i][k];
-                    trial[k+5]= hcsp4c[mem_sp4c[hcsp4c[i][j2]][j]][k];
+                    trial[k+1]= first_sp4c_cluster[k];
+                    trial[k+5]= hcsp4c[mem_sp4c[central_particle][j]][k];
                 }
-                quickSort(&trial[1],8);
+                quickSort(&trial[1],14);
 
                 flg=0;  // check trial cluster not already found
                 for (k=0; k < nBCC_15; ++k) {
@@ -109,6 +106,22 @@ void Clusters_GetBCC_15() {
         }
     }
 }
+
+int count_shared_sp4cs(int *neighbouring_sp4c_ids, const int first_sp4c_id, const int center_id) {
+    // Count how many sp4c's have a spindle in common with first_sp4c_id and get their ids
+    int num_shared_sp5b = 0;
+    for (int other_sp5b_pointer = 0; other_sp5b_pointer < nmem_sp5b[center_id]; ++other_sp5b_pointer) {
+        int other_sp5_id = mem_sp5b[center_id][other_sp5b_pointer];
+        if (other_sp5_id > first_sp5b_id) {
+            if (num_shared_sp5b < 9) {
+                neighbouring_sp5_ids[num_shared_sp5b] = other_sp5_id;
+            }
+            num_shared_sp5b++;
+        }
+    }
+    return num_shared_sp5b;
+}
+
 
 void Cluster_Write_BCC15() {
     int i;
